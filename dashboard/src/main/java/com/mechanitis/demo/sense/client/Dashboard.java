91f127ba131534191addbc6ac31f31c5ceae9be1
Trisha Gee <trisha.gee@gmail.com>
2015-02-20 11:28:50 +0100

    New start point for Dashboard

--- dashboard/src/main/java/com/mechanitis/demo/sense/client/Dashboard.java
+++ dashboard/src/main/java/com/mechanitis/demo/sense/client/Dashboard.java
@@ -1,26 +1,11 @@
 package com.mechanitis.demo.sense.client;
 
-import com.mechanitis.demo.sense.client.mood.HappinessChartData;
-import com.mechanitis.demo.sense.client.mood.MoodChartData;
-import com.mechanitis.demo.sense.client.mood.MoodsParser;
-import com.mechanitis.demo.sense.client.mood.TweetMood;
-import com.mechanitis.demo.sense.client.user.LeaderboardData;
-import com.mechanitis.demo.sense.infrastructure.ClientEndpoint;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import static java.net.URI.create;
-
 public class Dashboard extends Application {
-    private final ClientEndpoint<String> userClientEndpoint = new ClientEndpoint<>(create("ws://localhost:8083/users/"),
-                                                                 message -> message);
-    private final ClientEndpoint<TweetMood> moodClientEndpoint = new ClientEndpoint<>(create("ws://localhost:8082/moods/"),
-                                                                                      MoodsParser::parse);
-    private final MoodChartData moodData = new MoodChartData();
-    private final HappinessChartData happinessData = new HappinessChartData();
-    private final LeaderboardData leaderboardData = new LeaderboardData();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
