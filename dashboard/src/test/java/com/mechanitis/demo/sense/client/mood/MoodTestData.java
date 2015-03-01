01471afa32f5091a7a5e0004dabc2bca2ff8bdcf
Trisha Gee <trisha.gee@gmail.com>
2015-03-01 19:03:28 +0100

    Stopped cheating with the mood test data

--- dashboard/src/test/java/com/mechanitis/demo/sense/client/mood/MoodTestData.java
+++ dashboard/src/test/java/com/mechanitis/demo/sense/client/mood/MoodTestData.java
@@ -5,10 +5,10 @@ import com.mechanitis.demo.sense.client.StubService;
 import java.util.Random;
 
 public class MoodTestData {
-    private static final String[] POSSIBLE_MOODS = new String[]{"HAPPY", "HAPPY", "SAD", "HAPPY,SAD"};
+    private static final String[] POSSIBLE_MOODS = new String[]{"HAPPY", "SAD", "HAPPY,SAD"};
 
     public static void main(String[] args) {
         Random random = new Random();
-        new StubService("/moods/", 8082, () -> POSSIBLE_MOODS[random.nextInt(4)]).run();
+        new StubService("/moods/", 8082, () -> POSSIBLE_MOODS[random.nextInt(3)]).run();
     }
 }
