12128520797a5ee87d7080a572e9a1bf2165ee41
Trisha Gee <trisha.gee@gmail.com>
2015-04-10 19:28:40 +0200

    [Step 1] Leaderboard UI

--- dashboard/src/main/java/com/mechanitis/demo/sense/client/Dashboard.java
+++ dashboard/src/main/java/com/mechanitis/demo/sense/client/Dashboard.java
@@ -1,15 +1,31 @@
 package com.mechanitis.demo.sense.client;
 
+import com.mechanitis.demo.sense.client.mood.HappinessChartData;
+import com.mechanitis.demo.sense.client.mood.MoodChartData;
+import com.mechanitis.demo.sense.client.mood.MoodsParser;
+import com.mechanitis.demo.sense.client.mood.TweetMood;
+import com.mechanitis.demo.sense.client.user.LeaderboardData;
+import com.mechanitis.demo.sense.infrastructure.ClientEndpoint;
+import com.mechanitis.demo.sense.infrastructure.MessageHandler;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.net.URI;
+
+import static java.net.URI.create;
 
 public class Dashboard extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         // TODO: wire up the models to the services they're getting the data from
+        LeaderboardData leaderboardData = new LeaderboardData();
+        ClientEndpoint<String> userEndpoint = new ClientEndpoint<>(create("ws://localhost:8083/users/"),
+                twitterHandle -> twitterHandle);
+        userEndpoint.addListener(leaderboardData);
+        userEndpoint.connect();
 
         // initialise the UI
         FXMLLoader loader = new FXMLLoader(getClass().getResource("dashboard.fxml"));
@@ -19,6 +35,7 @@ public class Dashboard extends Application {
 
         // TODO: get each of the controllers and wire up the models
         DashboardController dashboardController = loader.getController();
+        dashboardController.getLeaderboardController().setData(leaderboardData);
 
         // let's go!
         primaryStage.setScene(scene);
--- dashboard/src/main/java/com/mechanitis/demo/sense/client/user/LeaderboardData.java
+++ dashboard/src/main/java/com/mechanitis/demo/sense/client/user/LeaderboardData.java
@@ -1,13 +1,18 @@
 package com.mechanitis.demo.sense.client.user;
 
+import com.mechanitis.demo.sense.infrastructure.MessageListener;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static javafx.collections.FXCollections.observableArrayList;
 
-public class LeaderboardData {
+public class LeaderboardData implements MessageListener<String> {
     private static final int NUMBER_OF_LEADERS = 18;
     private final Map<String, TwitterUser> allTwitterUsers = new HashMap<>();
 
@@ -15,5 +20,17 @@ public class LeaderboardData {
 
     public ObservableList<TwitterUser> getItems() {
         return items;
+    }
+
+    @Override
+    public void onMessage(String twitterHandle) {
+        TwitterUser twitterUser = allTwitterUsers.computeIfAbsent(twitterHandle, TwitterUser::new);
+        twitterUser.incrementCount();
+
+        List<TwitterUser> topTweeters = allTwitterUsers.values().stream()
+                .sorted(Comparator.comparing(TwitterUser::getTweets).reversed())
+                .limit(NUMBER_OF_LEADERS)
+                .collect(Collectors.toList());
+        Platform.runLater(() -> items.setAll(topTweeters));
     }
 }
