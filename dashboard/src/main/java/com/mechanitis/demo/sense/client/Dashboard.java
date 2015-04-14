13d12e8827c8d27f92939b2813a11bf8c70ed4fd
Trisha Gee <trisha.gee@gmail.com>
2015-04-14 14:02:21 +0200

    Simplified Dashboard so that I don't have to add the wiring during the demo

--- dashboard/src/main/java/com/mechanitis/demo/sense/client/Dashboard.java
+++ dashboard/src/main/java/com/mechanitis/demo/sense/client/Dashboard.java
@@ -1,5 +1,8 @@
 package com.mechanitis.demo.sense.client;
 
+import com.mechanitis.demo.sense.client.mood.HappinessChartData;
+import com.mechanitis.demo.sense.client.mood.MoodChartData;
+import com.mechanitis.demo.sense.client.user.LeaderboardData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,6 +12,11 @@ public class Dashboard extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // all models created in advance
+        LeaderboardData leaderboardData = new LeaderboardData();
+        MoodChartData moodChartData = new MoodChartData();
+        HappinessChartData happinessChartData = new HappinessChartData();
+
         // TODO: wire up the models to the services they're getting the data from
 
         // initialise the UI
@@ -17,8 +25,11 @@ public class Dashboard extends Application {
         Scene scene = new Scene(loader.load(), 1024, 1024);
         scene.getStylesheets().add("dashboard.css");
 
-        // TODO: get each of the controllers and wire up the models
+        // wire up the models to the controllers
         DashboardController dashboardController = loader.getController();
+        dashboardController.getLeaderboardController().setData(leaderboardData);
+        dashboardController.getMoodController().setData(moodChartData);
+        dashboardController.getHappinessController().setData(happinessChartData);
 
         // let's go!
         primaryStage.setScene(scene);
