59a028afbbb638e6fb6548ab11ecbe12663d2341
Trisha Gee <trisha.gee@gmail.com>
2015-06-08 22:27:50 +0200

    Lesson 3

--- sense/src/main/java/com/mechanitis/demo/sense/twitter/CannedTweetsService.java
+++ sense/src/main/java/com/mechanitis/demo/sense/twitter/CannedTweetsService.java
@@ -10,7 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.ExecutorService;
-import java.util.function.Consumer;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
 
@@ -41,11 +40,13 @@ public class CannedTweetsService implements Runnable {
     @Override
     public void run() {
         executor.submit(server);
-
-        // TODO: get a stream of lines in the file
-        // TODO: filter out "OK" noise
-        // TODO: send each line to be broadcast via websockets
-
+        try (Stream<String> lines = Files.lines(filePath)) {
+            lines.filter(s -> !s.equals("OK"))
+                 .peek(s1 -> addArtificialDelay())
+                 .forEach(tweetsEndpoint::onMessage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void addArtificialDelay() {
