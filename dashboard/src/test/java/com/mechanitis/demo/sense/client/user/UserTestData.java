b1c90ed6ab6f60b6453572fee316d0712313c79e
Philip Schwarz <philip.johann.schwarz@gmail.com>
2015-09-05 07:09:02 +0100

    Improve readability by extracting random sample twitter handle generation to a constant.

--- dashboard/src/test/java/com/mechanitis/demo/sense/client/user/UserTestData.java
+++ dashboard/src/test/java/com/mechanitis/demo/sense/client/user/UserTestData.java
@@ -3,14 +3,16 @@ package com.mechanitis.demo.sense.client.user;
 import com.mechanitis.demo.sense.client.StubService;
 
 import java.util.Random;
+import java.util.function.Supplier;
 
 public class UserTestData {
     private static final String[] EXAMPLE_HANDLES = {"aaa", "bbb", "ccc", "ddd", "eee", "fff", "gee", "ggg",
             "hhh", "iii", "jjj", "kkk", "lll", "mmm", "nnn", "ooo", "ppp", "qqq", "rrr", "sss", "ttt", "uuu", "vvv",
             "www", "xxx", "yyy", "zzz"};
 
+    private static final Supplier<String> RANDOM_SAMPLE_TWITTER_HANDLE = () -> EXAMPLE_HANDLES[new Random().nextInt(EXAMPLE_HANDLES.length)];
+
     public static void main(String[] args) {
-        Random random = new Random();
-        new StubService("/users/", 8083, () -> EXAMPLE_HANDLES[random.nextInt(EXAMPLE_HANDLES.length)]).run();
+        new StubService("/users/", 8083, RANDOM_SAMPLE_TWITTER_HANDLE).run();
     }
 }
