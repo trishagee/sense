943027c40278b3d4d2a9ab821ee145b3d283be7d
Philip Schwarz <philip.johann.schwarz@gmail.com>
2015-09-05 06:44:59 +0100

    Remove redundant code in sample twitter handles array initialiser.

--- dashboard/src/test/java/com/mechanitis/demo/sense/client/user/UserTestData.java
+++ dashboard/src/test/java/com/mechanitis/demo/sense/client/user/UserTestData.java
@@ -5,7 +5,7 @@ import com.mechanitis.demo.sense.client.StubService;
 import java.util.Random;
 
 public class UserTestData {
-    private static final String[] EXAMPLE_HANDLES = new String[]{"aaa", "bbb", "ccc", "ddd", "eee", "fff", "gee", "ggg",
+    private static final String[] EXAMPLE_HANDLES = {"aaa", "bbb", "ccc", "ddd", "eee", "fff", "gee", "ggg",
             "hhh", "iii", "jjj", "kkk", "lll", "mmm", "nnn", "ooo", "ppp", "qqq", "rrr", "sss", "ttt", "uuu", "vvv",
             "www", "xxx", "yyy", "zzz"};
 
