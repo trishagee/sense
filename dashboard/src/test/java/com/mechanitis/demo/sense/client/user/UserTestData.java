9f5c3bb824271ed024d9eb25bcfccc2d439a9bc7
Philip Schwarz <philip.johann.schwarz@gmail.com>
2015-09-06 14:02:22 +0100

    Remove duplication of random data generation logic.

--- dashboard/src/test/java/com/mechanitis/demo/sense/client/user/UserTestData.java
+++ dashboard/src/test/java/com/mechanitis/demo/sense/client/user/UserTestData.java
@@ -1,18 +1,14 @@
 package com.mechanitis.demo.sense.client.user;
 
+import com.mechanitis.demo.sense.client.RandomGenerator;
 import com.mechanitis.demo.sense.client.StubService;
-
-import java.util.Random;
-import java.util.function.Supplier;
 
 public class UserTestData {
     private static final String[] EXAMPLE_HANDLES = {"aaa", "bbb", "ccc", "ddd", "eee", "fff", "gee", "ggg",
             "hhh", "iii", "jjj", "kkk", "lll", "mmm", "nnn", "ooo", "ppp", "qqq", "rrr", "sss", "ttt", "uuu", "vvv",
             "www", "xxx", "yyy", "zzz"};
 
-    private static final Supplier<String> TWITTER_HANDLE_GENERATOR = () -> EXAMPLE_HANDLES[new Random().nextInt(EXAMPLE_HANDLES.length)];
-
     public static void main(String[] args) {
-        new StubService("/users/", 8083, TWITTER_HANDLE_GENERATOR).run();
+        new StubService("/users/", 8083, RandomGenerator.of(EXAMPLE_HANDLES)).run();
     }
 }
