2b5fc0974926cd41a6bee47d182e5ba94ff7843c
Philip Schwarz <philip.johann.schwarz@gmail.com>
2015-09-06 14:09:11 +0100

    Remove redundant type information.

--- dashboard/src/test/java/com/mechanitis/demo/sense/client/mood/MoodTestData.java
+++ dashboard/src/test/java/com/mechanitis/demo/sense/client/mood/MoodTestData.java
@@ -4,7 +4,7 @@ import com.mechanitis.demo.sense.client.RandomGenerator;
 import com.mechanitis.demo.sense.client.StubService;
 
 public class MoodTestData {
-    private static final String[] POSSIBLE_MOODS = new String[]{"HAPPY", "SAD", "HAPPY,SAD"};
+    private static final String[] POSSIBLE_MOODS = {"HAPPY", "SAD", "HAPPY,SAD"};
 
     public static void main(String[] args) {
         new StubService("/moods/", 8082, RandomGenerator.of(POSSIBLE_MOODS)).run();
