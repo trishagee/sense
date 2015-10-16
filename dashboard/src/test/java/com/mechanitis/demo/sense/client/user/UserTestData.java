365f65e1c5bab83b128b0acf8b63a6f57b6df1f1
Trisha Gee <trisha.gee@gmail.com>
2015-10-16 21:47:46 +0200

    More readable start point; includes IntelliJ settings.

--- dashboard/src/test/java/com/mechanitis/demo/sense/client/user/UserTestData.java
+++ dashboard/src/test/java/com/mechanitis/demo/sense/client/user/UserTestData.java
@@ -5,12 +5,14 @@ import com.mechanitis.demo.sense.client.StubService;
 import java.util.Random;
 
 public class UserTestData {
-    private static final String[] EXAMPLE_HANDLES = new String[]{"aaa", "bbb", "ccc", "ddd", "eee", "fff", "gee", "ggg",
-            "hhh", "iii", "jjj", "kkk", "lll", "mmm", "nnn", "ooo", "ppp", "qqq", "rrr", "sss", "ttt", "uuu", "vvv",
-            "www", "xxx", "yyy", "zzz"};
+    private static final String[] EXAMPLE_HANDLES = new String[]{"aaa", "bbb",
+            "ccc", "ddd", "eee", "fff", "gee", "ggg", "hhh", "iii", "jjj",
+            "kkk", "lll", "mmm", "nnn", "ooo", "ppp", "qqq", "rrr", "sss",
+            "ttt", "uuu", "vvv", "www", "xxx", "yyy", "zzz"};
 
     public static void main(String[] args) {
         Random random = new Random();
-        new StubService("/users/", 8083, () -> EXAMPLE_HANDLES[random.nextInt(27)]).run();
+        new StubService("/users/", 8083,
+                        () -> EXAMPLE_HANDLES[random.nextInt(27)]).run();
     }
 }
