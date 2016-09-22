4e8d25c09c7efd72b184cdcf52981e6187b357b0
Trisha Gee <trisha.gee@gmail.com>
2016-09-22 14:33:36 +0200

    Private methods on interfaces

--- sense/src/main/java/com/mechanitis/demo/sense/twitter/TweetParser.java
+++ sense/src/main/java/com/mechanitis/demo/sense/twitter/TweetParser.java
@@ -3,18 +3,18 @@ package com.mechanitis.demo.sense.twitter;
 public interface TweetParser {
 
     static String getTweetMessageFrom(String fullTweet) {
-        //very crude
         String textFieldName = "\"text\":\"";
-        String nextFieldName = "\",\"source\":\"";
-        int indexOfTextField = fullTweet.indexOf(textFieldName) + textFieldName.length();
-        int indexOfEndOfText = fullTweet.indexOf(nextFieldName);
-        return fullTweet.substring(indexOfTextField, indexOfEndOfText);
+        return getFieldValue(fullTweet, textFieldName);
     }
 
     static String getTwitterHandleFromTweet(String fullTweet) {
         String twitterHandleFieldName = "\"screen_name\":\"";
-        int indexOfTwitterHandleField = fullTweet.indexOf(twitterHandleFieldName)+ twitterHandleFieldName.length();
-        int indexOfEndOfTwitterHandle = fullTweet.indexOf("\"", indexOfTwitterHandleField);
-        return fullTweet.substring(indexOfTwitterHandleField, indexOfEndOfTwitterHandle);
+        return getFieldValue(fullTweet, twitterHandleFieldName);
+    }
+
+    private static String getFieldValue(String fullTweet, String fieldName) {
+        int indexOfTextField = fullTweet.indexOf(fieldName) + fieldName.length();
+        int indexOfEndOfText = fullTweet.indexOf("\"", indexOfTextField);
+        return fullTweet.substring(indexOfTextField, indexOfEndOfText);
     }
 }
