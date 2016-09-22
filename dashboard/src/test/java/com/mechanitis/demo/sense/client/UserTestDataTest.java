bf5af4b1dda1dd3257a217e60456b38d15efe33b
Trisha Gee <trisha.gee@gmail.com>
2016-09-22 21:46:17 +0200

    Deprecation in Java 9

---
+++ dashboard/src/test/java/com/mechanitis/demo/sense/client/UserTestDataTest.java
@@ -0,0 +1,13 @@
+package com.mechanitis.demo.sense.client;
+
+import com.mechanitis.demo.sense.client.user.UserTestData;
+import org.junit.Test;
+
+@SuppressWarnings("deprecation")
+public class UserTestDataTest {
+    @Test
+    public void shouldUSeDeprecatedProperly() {
+        UserTestData userTestData = new UserTestData();
+    }
+
+}
