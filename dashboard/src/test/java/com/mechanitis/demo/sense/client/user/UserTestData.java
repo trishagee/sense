7119f30c2e837caca426dde59933a8b49c25c62a
Trisha Gee <trisha.gee@gmail.com>
2016-11-10 12:20:49 +0100

    Demo Set instead of List

--- dashboard/src/test/java/com/mechanitis/demo/sense/client/user/UserTestData.java
+++ dashboard/src/test/java/com/mechanitis/demo/sense/client/user/UserTestData.java
@@ -2,17 +2,34 @@ package com.mechanitis.demo.sense.client.user;
 
 import com.mechanitis.demo.sense.client.StubService;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class UserTestData {
-    private static final String[] EXAMPLE_HANDLES = new String[]{"aaa", "bbb",
-            "ccc", "ddd", "eee", "fff", "gee", "ggg", "hhh", "iii", "jjj",
-            "kkk", "lll", "mmm", "nnn", "ooo", "ppp", "qqq", "rrr", "sss",
-            "ttt", "uuu", "vvv", "www", "xxx", "yyy", "zzz"};
+    private static final Set<String> EXAMPLE_HANDLES = new HashSet<>(Arrays.asList("aaa", "bbb", "ccc", "ddd", "eee",
+                                                                                   "fff", "gee", "ggg", "hhh", "iii",
+                                                                                   "jjj", "kkk", "lll", "mmm", "nnn",
+                                                                                   "ooo", "ppp", "qqq", "rrr", "sss",
+                                                                                   "ttt", "uuu", "vvv", "www", "xxx",
+                                                                                   "yyy", "zzz"));
 
     public static void main(String[] args) {
-        Random random = new Random();
         new StubService("/users/", 8083,
-                        () -> EXAMPLE_HANDLES[random.nextInt(27)]).run();
+                        UserTestData::getRandomValue).run();
+    }
+
+    private static String getRandomValue() {
+        Random random = new Random();
+
+        int requiredIndex = random.nextInt(27);
+        int i = 0;
+        for (String handle : EXAMPLE_HANDLES) {
+            if (i == requiredIndex)
+                return handle;
+            i = i + 1;
+        }
+        return null;
     }
 }
